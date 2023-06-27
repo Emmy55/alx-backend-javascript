a09f6d835dbb85d1ed9e168dd2fae3db14ce04ad
Emmanuel <nkememmanuel5505@gmail.com>
2023-06-27 05:40:17 -0700

    task04

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,3 @@
-export defaukt function signUpUser(firstName, lastName) {
-	return Promise.resolve({ firstName, lastName });
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName, lastName });
 }
